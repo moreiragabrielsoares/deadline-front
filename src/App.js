de744e8323395e99e7e60aaad7b312db28a75d42
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-10-05 10:11:12 -0300

    feat: add route /login to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import SingUpPage from "./pages/SingUpPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<SingUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
