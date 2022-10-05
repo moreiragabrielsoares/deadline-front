2787d27f771077b4e7465c3a461f17bcbab2ce3c
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-10-05 10:00:00 -0300

    feat: add route /signup to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import SingUpPage from "./pages/SingUpPage";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SingUpPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
