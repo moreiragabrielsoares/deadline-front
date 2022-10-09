15b3b56ad3225723cb992fad461849bccf6ed92a
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-10-09 19:22:10 -0300

    feat: add ProcessePage to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import SingUpPage from "./pages/SingUpPage";
 import LoginPage from "./pages/LoginPage";
+import ProcessesPage from "./pages/ProcessesPage";
 import MainPage from "./pages/MainPage";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/signup" element={<SingUpPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<MainPage />} />
+        <Route path="/processes" element={<ProcessesPage />} />
       </Routes>
     </BrowserRouter>
   );
