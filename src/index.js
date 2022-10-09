def82ebdf68cb31eccdfc05050c3c09b0b6ddccb
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-10-09 19:13:02 -0300

    fix: remove react strict mode

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./assets/css/reset.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+  </>
 );
